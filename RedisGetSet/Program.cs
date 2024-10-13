5fb8d8b8aec3c6ec30c7fadcbe2abf50c8d9bb62
Swapnil5390 <pawar3382@gmail.com>
2024-10-13 13:03:36 +0530

    Add project files.

---
+++ RedisGetSet/Program.cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+
+
+namespace RedisGetSet
+{
+    internal class Program
+    {
+        public static string redisServer = "localhost";
+        static void Main(string[] args)
+        {
+            //1.Identity redis server URL
+            //2. Imports stack exchange libary for redis connectivity using c#
+            //3.Create  ConnectionMultiplexer object for 
+            ConnectionMultiplexer conObject = ConnectionMultiplexer.Connect(redisServer);
+
+            IDatabase db = conObject.GetDatabase();
+
+            db.StringSet("Gender", "Male");
+            db.StringGet("Gender");
+
+            Console.WriteLine(db.StringGet("Gender"));
+        }
+    }
+}
